34ddcb476511f9504f7847414ca3539a79e82853
viking0312 <m.maulik96@gmail.com>
2023-07-25 22:21:54 +0530

    Update ChatEndpoint.java

--- src/main/java/com/maulik/java/websocketchatapp/ChatEndpoint.java
+++ src/main/java/com/maulik/java/websocketchatapp/ChatEndpoint.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-//webssocket server
 @ServerEndpoint(value = "/chat/{username}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
 public class ChatEndpoint {
 
