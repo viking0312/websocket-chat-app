30bd650c799b53b32df68262f8068279570d4f66
viking0312 <m.maulik96@gmail.com>
2023-07-27 01:57:34 +0530

    Chat UI changes

--- src/main/webapp/websocket.js
+++ src/main/webapp/websocket.js
@@ -6,21 +6,39 @@ function connect() {
     var host = document.location.host;
     var pathname = document.location.pathname;
 
-    ws = new WebSocket("wss://" +host  + pathname + "chat/" + username);
+    ws = new WebSocket("ws://" +host  + pathname + "chat/" + username);
 
     ws.onmessage = function(event) {
-        var log = document.getElementById("log");
-        console.log(event.data.content);
+        // var log = document.getElementById("log");
+        // console.log(event.data.content);
         var message = JSON.parse(event.data);
-        log.innerHTML += message.from + " : " + message.content + "\n";
+        // log.innerHTML += message.from + " : " + message.content + "\n";
+
+        var iDiv = document.createElement('div');
+        // iDiv.id = 'block';
+        iDiv.className = 'm-1 chat-message';
+        // iDiv.textContent = message.content;
+
+        const senderSpan = document.createElement("span");
+        senderSpan.classList.add("font-weight-bold");
+        senderSpan.textContent = message.from + ": ";
+
+        const messageSpan = document.createElement("span");
+        messageSpan.textContent = message.content;
+        iDiv.appendChild(senderSpan);
+        iDiv.appendChild(messageSpan);
+
+        document.getElementById('chat-box').appendChild(iDiv);
+
     };
 }
 
 function send() {
     var content = document.getElementById("msg").value;
-    var json = JSON.stringify({
-        "content":content
-    });
+    // var json = JSON.stringify({
+    //     "content":content
+    // });
 
-    ws.send(json);
+    ws.send(content);
+    document.getElementById("msg").value = "";
 }
